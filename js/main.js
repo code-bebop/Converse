492d0f2ead3024f8551c9efcc17ae95f8502ea26
code-bebop <57097064+code-bebop@users.noreply.github.com>
2020-06-16 16:20:34 +0900

    2020-06-16 16:17
    - header에 sticky 포지션 지정, js에서 header의 sticky여부를 감지해서 class 부여
    - 화면 좌측 하단에 fixed되어있는 버튼 추가

--- js/main.js
+++ js/main.js
@@ -3,6 +3,17 @@ function searchHandler() {
     $(".search__btn").toggleClass("opened");
     $(".search__dashboard").toggleClass("opened");
     $(".search__layer").toggleClass("opened");
+    $("body").toggleClass("overflow");
+  })
+  
+  $(window).scroll((e)=>{
+    if($(document).scrollTop() > $(".notice").height()) {
+      $(".header").addClass("is-sticky");
+      $(".search__btn").addClass("is-sticky");
+    } else if ($(document).scrollTop() < $(".notice").height()) {
+      $(".header").removeClass("is-sticky");
+      $(".search__btn").removeClass("is-sticky");
+    }
   })
 }
 
